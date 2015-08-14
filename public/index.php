8bf34be883e0af3fac67d59f936aa7fedbb262d8
backcut <2214737069@qq.com>
2015-08-14 15:24:06 +0800

    baobao

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
-echo 'test';
-
+include('header.php');
 ?>
+<style type='text/css'>
+</style>
+</head> 
+<body>
+<h1>你好，我是欧哲源</h1>
+<p>我从2015年8月14日开始学习编程序</p>
+<hr />
+<p>这里会记录我的学习收获</p>
+</body>
+</html>
