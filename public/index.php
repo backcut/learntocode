da4e448544798252679dc5edf81e3f82bf233c1b
backcut <2214737069@qq.com>
2015-08-14 17:02:31 +0800

    baobao

--- public/index.php
+++ public/index.php
@@ -7,16 +7,16 @@ include('include/header.php');
 </head> 
 <body>
  
-<h1 style=‘color:blue;’>你好，我是欧哲源</h1>
-<p><img src=‘image/me.jpg’ /></p>
+<h1 style='color:blue;'>你好，我是欧哲源</h1>
+<p><img src='image/me.jpg'/></p>
 
-<p>我从2015年8月14日开始学习编程序</p>
+<p class='color:green'>我从2015年8月14日开始学习编程序</p>
 <p>我的老师是我哥哥</p>
 <hr />
 <p>这里会记录我的学习收获</p>
 今天我学到很多关于电脑的很多知识
 晚上我还要继续研究，现在还要很多东西要去学习
 <hr />
-
+现在时间：<?php echo date('Y-md-d H:i:s'); ?>
 </body>
 </html>
