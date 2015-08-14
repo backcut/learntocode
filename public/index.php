a9243a38b4d64ef8f7ed4bb12ec73d8cc5551a56
backcut <2214737069@qq.com>
2015-08-14 17:07:56 +0800

    baobao

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ include('include/header.php');
 <body>
  
 <h1 style='color:blue;'>你好，我是欧哲源</h1>
-<p><img src='image/me.jpg'/></p>
+<p><img src='/image/me.jpg'/></p>
 
 <p class='color:green'>我从2015年8月14日开始学习编程序</p>
 <p>我的老师是我哥哥</p>
